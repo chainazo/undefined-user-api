a70c2ff98dbd13ffe57338aae9a7b1d7d02b4906
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-12-25 21:15:53 +0900

    implemented uuid generation

--- user_api/models.py
+++ user_api/models.py
@@ -1,13 +1,16 @@
+import uuid
 from django.db import models
 
 
 class UserModel(models.Model):
-    name = models.CharField(max_length=50)
+    user_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid1, editable=False)
+    name = models.CharField(max_length=50, default=None)
     email = models.EmailField()
-    password = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=13)
+    password = models.CharField(max_length=20, default=None)
+    phone_number = models.CharField(max_length=13, default=None, unique=True)
     age = models.IntegerField()
-    gender = models.CharField(max_length=1)
+    gender = models.CharField(max_length=1, default='M')
 
     def __str__(self):
         return 'User Model\nName: {}\nEmail: {}\nAge: {}\nGender:\n'.\
--- user_api/views.py
+++ user_api/views.py
@@ -3,15 +3,13 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from user_api.models import UserModel
-from user_api.serializers import UserSerializer, UserCreateSerializer
+from user_api.serializers import UserSerializer, UserCreateSerializer, LoginSerializer, LoginResultSerializer
 
 
 class UserView(APIView):
     """
     View for all users
     """
-    def __str__(self):
-        return 'API View for all users'
 
     def get(self, request, format=None):
         """
@@ -42,16 +40,14 @@ class UserDetailView(APIView):
     """
     View for single user
     """
-    def __str__(self):
-        return 'API View for single user'
 
-    def _get_object(self, pk):
+    def _get_object(self, user_id):
         try:
-            return UserModel.objects.get(pk=pk)
+            return UserModel.objects.get(user_id=user_id)
         except UserModel.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk, format=None):
+    def get(self, request, user_id, format=None):
         """
         GET - Single user detail
         :param request: http request
@@ -59,6 +55,35 @@ class UserDetailView(APIView):
         :param pk: primary key of object
         :return: json object containing single user detail
         """
-        user_model = self._get_object(pk=pk)
+        user_model = self._get_object(user_id=user_id)
         user_serializer = UserSerializer(user_model)
         return Response(user_serializer.data)
+
+
+class LoginView(APIView):
+    """
+    View for login result
+    """
+
+    def post(self, request):
+        """
+        POST - Login method
+        :param request: http request
+        :return: user's uuid if successful, http 400 if error
+        """
+
+        request_serializer = LoginSerializer(data=request.data)
+        if request_serializer.is_valid() is False:
+            return Response(request_serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+        matching_models = UserModel.objects.filter(
+            name=request.data['name']).filter(password=request.data['password'])
+        if len(matching_models) == 0:
+            return Response({'error': 'ID or password is incorrect'}, status.HTTP_400_BAD_REQUEST)
+
+        for model in matching_models:
+            compare_serializer = LoginSerializer(model)
+            if request_serializer.data == compare_serializer.data:
+                return Response(LoginResultSerializer(model).data, status.HTTP_200_OK)
+
+        return Response({'error': 'ID or password is incorrect'}, status.HTTP_400_BAD_REQUEST)
