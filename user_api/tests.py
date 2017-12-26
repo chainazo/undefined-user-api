caaf6f485b887cb2f45989f4ff58ca609273ecd0
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-12-26 19:25:21 +0900

    added tests for LoginResultSerializer, UserView(POST)

--- user_api/tests.py
+++ user_api/tests.py
@@ -7,92 +7,60 @@ from user_api.serializers import UserSerializer, UserCreateSerializer, LoginResu
 from user_api.views import UserView, UserDetailView, LoginView
 
 
-# Test for serializers
-class UserSerializerTest(TestCase):
-    """
-    Test for user serializer(without uuid, password, phone number)
-    """
-
-    def setUp(self):
-        self.test_model = UserModel(name='UserSerializer Test', email='ustest@test.com',
-                                    password='testpwd111', phone_number='010-1234-5678',
-                                    age=20, gender='W')
-        self.test_model.save()
-        self.test_serializer = UserSerializer(self.test_model).data
-
-    def test_uuid_is_not_included(self):
-        flag = False
-        try:
-            self.test_serializer['user_id']
-        except KeyError:
-            flag = True
-        self.assertTrue(flag)
-
-    def test_password_is_not_included(self):
-        flag = False
-        try:
-            self.test_serializer['password']
-        except KeyError:
-            flag = True
-        self.assertTrue(flag)
-
-    def test_phone_number_is_not_included(self):
-        flag = False
-        try:
-            self.test_serializer['phone_number']
-        except KeyError:
-            flag = True
-        self.assertTrue(flag)
-
-    def test_everything_else_is_added_correctly(self):
-        self.assertEqual(self.test_model.name, self.test_serializer['name'])
-        self.assertEqual(self.test_model.email, self.test_serializer['email'])
-        self.assertEqual(self.test_model.age, self.test_serializer['age'])
-        self.assertEqual(self.test_model.gender,
-                         self.test_serializer['gender'])
-
-
 class UserCreateSerializerTest(TestCase):
     """
     Test for user create serializer(with everything)
     """
-    # TODO test with invalid data
 
     def setUp(self):
-        self.test_model = UserModel(name='UserCreateSerializer Test', email='ucstest@test.com',
-                                    password='testpwd222', phone_number='010-9012-3456',
-                                    age=21, gender='M')
-        self.test_model.save()
-        self.test_serializer = UserCreateSerializer(self.test_model).data
+        self.data = {
+            'name': 'Test',
+            'email': 'test@test.com',
+            'password': 'Test1234',
+            'phone_number': '010-1234-5678',
+            'age': 20,
+            'gender': 'M'
+        }
 
-    def test_is_serializer_generated_correctly(self):
-        self.assertEqual(str(self.test_model.user_id),
-                         self.test_serializer['user_id'])
-        self.assertEqual(self.test_model.name, self.test_serializer['name'])
-        self.assertEqual(self.test_model.email, self.test_serializer['email'])
-        self.assertEqual(self.test_model.password,
-                         self.test_serializer['password'])
-        self.assertEqual(self.test_model.phone_number,
-                         self.test_serializer['phone_number'])
-        self.assertEqual(self.test_model.age, self.test_serializer['age'])
-        self.assertEqual(self.test_model.gender,
-                         self.test_serializer['gender'])
+    def test_invalid_password_length(self):
+        self.data['password'] = 'Asdf123'
+        test_serializer = UserCreateSerializer(data=self.data)
+        self.assertFalse(test_serializer.is_valid())
 
+    def test_invalid_password_no_number(self):
+        self.data['password'] = 'Asdfasdf'
+        test_serializer = UserCreateSerializer(data=self.data)
+        self.assertFalse(test_serializer.is_valid())
 
-class LoginResultSerializerTest(TestCase):
-    """
-    Test for login result serializer(with uuid only)
-    """
+    def test_invalid_password_no_uppercase(self):
+        self.data['password'] = 'asdf1234'
+        test_serializer = UserCreateSerializer(data=self.data)
+        self.assertFalse(test_serializer.is_valid())
 
-    def setUp(self):
-        self.test_model = UserModel(name='LoginResultSerializer Test', email='lrstest@test.com',
-                                    password='testpwd333', phone_number='010-7890-1234',
-                                    age=22, gender='W')
-        self.test_serializer = LoginResultSerializer(self.test_model).data
+    def test_not_a_phone_number(self):
+        self.data['phone_number'] = '12345678'
+        test_serializer = UserCreateSerializer(data=self.data)
+        self.assertFalse(test_serializer.is_valid())
 
-    def test_is_serializer_generated_correctly(self):
-        self.assertEqual(str(self.test_model.user_id),
-                         self.test_serializer['user_id'])
+    def test_invalid_phone_number_invalid_prefix(self):
+        self.data['phone_number'] = '011-1234-5678'
+        test_serializer = UserCreateSerializer(data=self.data)
+        self.assertFalse(test_serializer.is_valid())
+
+    def test_invalid_phone_number_invalid_length(self):
+        self.data['phone_number'] = '011-234-5678'
+        test_serializer = UserCreateSerializer(data=self.data)
+        self.assertFalse(test_serializer.is_valid())
+
+    def test_invalid_gender(self):
+        self.data['gender'] = 'X'
+        test_serializer = UserCreateSerializer(data=self.data)
+        self.assertFalse(test_serializer.is_valid())
+
+    def test_valid_create(self):
+        test_serializer = UserCreateSerializer(data=self.data)
+        self.assertTrue(test_serializer.is_valid())
+        self.assertEqual(test_serializer.data, self.data)
 
 
 # Test for views
@@ -137,14 +105,69 @@ class UserViewPostTest(TestCase):
     """
     Test for UserView -post
     """
-    pass
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.data = {
+            'name': 'Test',
+            'email': 'test@test.com',
+            'password': 'Test1234',
+            'phone_number': '010-1234-5678',
+            'age': 20,
+            'gender': 'M'
+        }
+        self.view = UserView.as_view()
 
+    def test_post_without_auth(self):
+        """
+        Test case 1: no credentials provided
+        Expected result: HTTP 401
+        """
+        request = self.factory.post('/users/', data=self.data)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-class UserDetailViewTest(TestCase):
-    """
-    Test for UserDetailView - get
-    """
-    pass
+    def test_post_with_invalid_auth(self):
+        """
+        Test case 2: Invalid credentials provided
+        Expected result: HTTP 401
+        """
+        request = self.factory.post('/users/', data=self.data)
+        request.user = AnonymousUser()
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_post_with_insufficient_args(self):
+        """
+        Test case 3: Arguments not totally filled
+        Expected result: HTTP 400
+        """
+        invalid_data = {}
+        request = self.factory.post('/users/', data=invalid_data)
+        force_authenticate(request, user=User)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_with_invalid_args(self):
+        """
+        Test case 4: Arguments are invalid
+        Expected result: HTTP 400
+        """
+        self.data['password'] = 'abc123'
+        self.data['phone_number'] = '011-2345-6789'
+        request = self.factory.post('/users/', data=self.data)
+        force_authenticate(request, user=User)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_wth_valid_args(self):
+        """
+        Test case 5: Everything is valid
+        Expected result: HTTP 201
+        """
+        request = self.factory.post('/users/', data=self.data)
+        force_authenticate(request, user=User)
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
 class LoginViewTest(TestCase):
@@ -158,7 +181,7 @@ class LoginViewTest(TestCase):
 
         # saving sample models
         self.sample_model = UserModel(name='test', email='test@test.com',
-                                      password='testpwd', phone_number='010-1234-5678',
+                                      password='Testpwd!999', phone_number='010-1234-5678',
                                       age=20, gender='M')
         self.sample_model.save()
 
@@ -168,7 +191,7 @@ class LoginViewTest(TestCase):
         Expected result: HTTP 401
         """
         request = self.factory.post(
-            '/sessions/', {'name': 'test', 'password': 'testpwd'}, format='json')
+            '/sessions/', {'name': 'test', 'password': 'Testpwd!999'})
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
@@ -178,7 +201,7 @@ class LoginViewTest(TestCase):
         Expected result: HTTP 401
         """
         request = self.factory.post(
-            '/sessions/', {'name': 'test', 'password': 'testpwd'}, format='json')
+            '/sessions/', {'name': 'test', 'password': 'Testpwd!999'})
         request.user = AnonymousUser()
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -191,7 +214,7 @@ class LoginViewTest(TestCase):
         Expected result: HTTP 400
         """
         request = self.factory.post(
-            '/sessions/', {'name': 'foo', 'password': 'testpwd'}, format='json')
+            '/sessions/', {'name': 'foo', 'password': 'Testpwd!999'})
         force_authenticate(request, user=User)
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -202,7 +225,7 @@ class LoginViewTest(TestCase):
         Expected result: HTTP 400
         """
         request = self.factory.post(
-            '/sessions/', {'name': 'test', 'password': 'bar'}, format='json')
+            '/sessions/', {'name': 'test', 'password': 'bar'})
         force_authenticate(request, user=User)
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -213,7 +236,7 @@ class LoginViewTest(TestCase):
         Expected result: HTTP 200
         """
         request = self.factory.post(
-            '/sessions/', {'name': 'test', 'password': 'testpwd'}, format='json')
+            '/sessions/', {'name': 'test', 'password': 'Testpwd!999'})
         force_authenticate(request, user=User)
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
